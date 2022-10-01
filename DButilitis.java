ab1b8928434723c45567d599a3bf8170f3b59692
saikiran <saikiran9665@gmail.com>
2022-10-01 12:42:18 +0530

    my edit commit

--- DButilitis.java
+++ DButilitis.java
@@ -1,6 +1,5 @@
  this file content java code
-<<<<<<< HEAD
  this code is enterd by python
-=======
+
 this edit by git hub
->>>>>>> f1356c863e76d60debd164df0608ae6568c04b91
+ this line enterd by sai
